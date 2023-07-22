3b7f5731ff8353c28003b4481a73dd78540d9ffe
Joseph Kelley <josephkelley91@gmail.com>
2023-07-22 11:38:08 -0400

    CODE CHALLENGE 1 - Adds Next and Previous button click functionality to provide the infinite scrolling experience. Adds or removes carousel items from array as applicable to reduce the possibility of waste (hundreds or repeat carousel items). Adds an additional four items if the user is at the end of the set and an additional four have not already been added. If the user is at the end of the set and an additional four items have already been added, remove the additional four. Scroll position updated appropriately in all scenarios.

--- code-challenge-1/scripts/code-challenge-1.js
+++ code-challenge-1/scripts/code-challenge-1.js
@@ -1,11 +1,54 @@
 document.readyState === 'loading' ? document.addEventListener('DOMContentLoaded', documentReady()) : documentReady();
 
 function documentReady() {
+    const carousel = document.getElementById('carousel');
+    const carouselHTML = document.getElementById('carousel').innerHTML;
+
     document.getElementById('previous-button').addEventListener('click', function () {
-        document.getElementById('carousel').scrollLeft -= 300;
+        let carouselItems = document.getElementsByClassName('carousel-item');
+        let carouselItemsToAdjustForRendering = [];
+        for (let i = 0; i < carouselItems.length; i++) {
+            carouselItemsToAdjustForRendering.push(carouselItems[i]);
+        }
+        if (carousel.scrollLeft === 0) {
+            if (carouselItemsToAdjustForRendering.length >= 8) {
+                for (let i = 0; i < 4; i++) {
+                    carouselItemsToAdjustForRendering.pop();
+                }
+                carousel.innerHTML = '';
+                carouselItemsToAdjustForRendering.forEach(function (item) {
+                    carousel.innerHTML += item.outerHTML;
+                });
+            } else {
+                carousel.innerHTML += carouselHTML;
+            }
+            carousel.scrollLeft = 900;
+        } else {
+            carousel.scrollLeft -= 300;
+        }
     });
 
     document.getElementById('next-button').addEventListener('click', function () {
-        document.getElementById('carousel').scrollLeft += 300;
+        let carouselItems = document.getElementsByClassName('carousel-item');
+        let carouselItemsToAdjustForRendering = [];
+        for (let i = 0; i < carouselItems.length; i++) {
+            carouselItemsToAdjustForRendering.push(carouselItems[i]);
+        }
+        if (carousel.scrollLeft === 900) {
+            if (carouselItemsToAdjustForRendering.length >= 8) {
+                for (let i = 0; i < 4; i++) {
+                    carouselItemsToAdjustForRendering.shift();
+                }
+                carousel.innerHTML = '';
+                carouselItemsToAdjustForRendering.forEach(function (item) {
+                    carousel.innerHTML += item.outerHTML;
+                });
+            } else {
+                carousel.innerHTML += carouselHTML;
+            }
+            carousel.scrollLeft = 0;
+        } else {
+            carousel.scrollLeft += 300;
+        }
     });
 };
