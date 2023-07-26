f85f560e16400cf075d2935505f3ddbc3bb7813d
Joseph Kelley <josephkelley91@gmail.com>
2023-07-25 20:22:54 -0400

    CODE CHALLENGE 2 - Adds logic to add new list item on click of add new button, if input field has a value. Then resets add new input field's value to empty.

--- code-challenge-2/scripts/code-challenge-2.js
+++ code-challenge-2/scripts/code-challenge-2.js
@@ -1,5 +1,27 @@
 document.readyState === 'loading' ? document.addEventListener('DOMContentLoaded', documentReady()) : documentReady();
 
 function documentReady() {
+    const list = document.getElementById('list');
+    let listHTML = document.getElementById('list').innerHTML;
+
+    document.getElementById('add-new-button').addEventListener('click', function () {
+        const addNewInput = document.getElementById('add-new-input');
+        const addNewInputValue = addNewInput.value.trim();
+        if (addNewInputValue !== '') {
+            list.innerHTML += `<div class="list-item">
+                <input type="checkbox" class="list-item-checkbox" />
+                <div class="list-item-text">${addNewInputValue}</div>
+                <div class="list-item-buttons">
+                    <button class="list-item-button" id="edit-button">
+                        <img src="images/edit.svg" />
+                    </button>
+                    <button class="list-item-button" id="delete-button">
+                        <img src="images/delete.svg" />
+                    </button>
+                </div>
+            </div>`;
+            addNewInput.value = '';
+        }
+    });
 
 };
